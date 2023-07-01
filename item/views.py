10d51ed131bf9040d66d662153d4a6b2a46b467c
Garrodes <batuque77mlh@gmail.com>
2023-07-01 19:03:49 +0200

    UD :  !PB  img not deleted from media file on D

--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 from .models import Item
 
 # Create your views here.
@@ -30,4 +30,29 @@ def new(request):
     return render(request, 'item/form.html',{
                   'form':form,
                   'title':'Adding Items'
+    })
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk , created_by = request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
+
+@login_required
+def edit(request, pk ):
+    item = get_object_or_404(Item, pk=pk , created_by = request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+        if form.is_valid : 
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html',{
+                  'form':form,
+                  'title':'Edit Items'
     })
