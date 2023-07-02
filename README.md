84863a631661c84061eac5bfc985f1bf28d3309d
Garrodes <batuque77mlh@gmail.com>
2023-07-02 16:05:14 +0200

    conversation app   + readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+ Une petite app CRUD pour prendre en main django réalisée en suivant un tutoriel  de FreeCodeCamp : <br>
+ lien vidéo : https://youtu.be/ZxMB6Njs3ck <br>
+ lien code : https://github.com/SteinOveHelset/puddle <br>
--- puddle/settings.py
+++ puddle/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@rww7@88izz@vkuh&%p18-d0_^!kbc2hwn_-0r9^appf(#jvme'
+with open("../secret_key.txt") as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     'core',
     'item',
     'dashboard',
+    'conversation',
 ]
 
 MIDDLEWARE = [
